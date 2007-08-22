51750a896bd7eb02c0433c168a7760b45d341e39
Andrea Rossato <andrea.rossato@unibz.it>
2007-08-22 03:38:14 -0700

    added hmanage
    darcs-hash:20070822103814-32816-d53796834f86fb60471e83ebb2ff1a6716d85c19.gz

---
+++ src/hmanage.hs
@@ -0,0 +1,45 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  hmanage
+-- Copyright   :  (c) Andrea Rossato
+-- License     :  GPL2
+-- 
+-- Maintainer  :  Andrea Rossato <andrea.rossato@unibz.it>
+-- Stability   :  unstable
+-- Portability :  unportable
+--
+--  Toggle override_redirect for an X window
+--
+-----------------------------------------------------------------------------
+module Main where
+
+import Graphics.X11.Xlib
+import Graphics.X11.Xlib.Extras
+import System.Environment
+
+usage :: String -> String
+usage n = "Usage: " ++ n ++ " manage/unmanage windowID"
+
+main :: IO ()
+main = do
+  args <- getArgs
+  pn <- getProgName
+  let (win,ac) = case args of
+                   [] -> error $ usage pn
+                   w -> case (w !!0) of 
+                          "manage" -> (window, False)
+                          "unmanage" ->  (window, True)
+                          _ -> error $ usage pn
+                       where window = case  (w !! 1) of 
+                                               [] -> error $ usage pn
+                                               x -> read x :: Window
+  dpy <- openDisplay ""
+  unmapWindow dpy win
+  sync dpy False
+  allocaSetWindowAttributes $
+       \attributes -> do
+         set_override_redirect attributes ac
+         changeWindowAttributes dpy win cWOverrideRedirect attributes
+  mapWindow dpy win
+  sync dpy False
+
