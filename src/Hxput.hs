55a00532e7ad89065bff8b8d0c8b694dd625cb0a
Matthew Sackman matthew@wellquite.org <none>
2007-09-12 09:39:52 -0700

    Refactored and placed in a loop. No attempt to deal with downgrading the type of the selection yet.
    darcs-hash:20070912163952-af2b7-8fa04a5df1e9ad2fc4bfff5bb3fe7419b63f85f6.gz

--- src/Hxput.hs
+++ src/Hxput.hs
@@ -17,11 +17,13 @@ module Main where
 import Graphics.X11.Xlib
 import Graphics.X11.Xlib.Extras
 import System.Exit (exitWith, ExitCode(..))
+import System.Environment
 
 import Data.Char
 
 main :: IO ()
-main = do 
+main = do
+  (text:_) <- getArgs
   dpy <- openDisplay ""
   let dflt = defaultScreen dpy
   rootw  <- rootWindow dpy dflt
@@ -30,30 +32,33 @@ main = do
   ty <- internAtom dpy "UTF8_STRING" False
   xSetSelectionOwner dpy p win currentTime
   winOwn <- xGetSelectionOwner dpy p
-  print win
-  print ty
   if winOwn == win
-    then putStrLn "Yes"
-    else putStrLn "No"
-  allocaXEvent $ \e -> do
-    nextEvent dpy e
-    ev <- getEvent e
-    if ev_event_type ev == selectionRequest
-       then do print ev
-               -- selection == eg PRIMARY
-               -- target == type eg UTF8
-               -- property == property name or None
-               allocaXEvent $ \replyPtr -> do
-                         changeProperty8 (ev_event_display ev)
-                                         (ev_requestor ev)
-                                         (ev_property ev)
-                                         ty
-                                         propModeReplace
-                                         (map (fromIntegral . ord) $ "Boo")
-                         setSelectionNotify replyPtr (ev_requestor ev) (ev_selection ev) (ev_target ev) (ev_property ev) (ev_time ev)
-                         sendEvent dpy (ev_requestor ev) False noEventMask replyPtr
-               sync dpy False
-               putStrLn "Sent"
-       else do putStrLn "Failed!"
-  destroyWindow dpy win
-  exitWith ExitSuccess
+    then do allocaXEvent $ processEvent dpy ty text
+            destroyWindow dpy win
+            exitWith ExitSuccess
+    else do putStrLn "Unable to obtain ownership of the selection"
+            destroyWindow dpy win
+            exitWith (ExitFailure 1)
+
+processEvent dpy ty text e = do
+  nextEvent dpy e
+  ev <- getEvent e
+  if ev_event_type ev == selectionRequest
+    then do print ev
+            -- selection == eg PRIMARY
+            -- target == type eg UTF8
+            -- property == property name or None
+            allocaXEvent $ \replyPtr -> do
+                   changeProperty8 (ev_event_display ev)
+                                   (ev_requestor ev)
+                                   (ev_property ev)
+                                   ty
+                                   propModeReplace
+                                   (map (fromIntegral . ord) text)
+                   setSelectionNotify replyPtr (ev_requestor ev) (ev_selection ev) (ev_target ev) (ev_property ev) (ev_time ev)
+                   sendEvent dpy (ev_requestor ev) False noEventMask replyPtr
+            sync dpy False
+            putStrLn "Sent"
+    else do putStrLn "Unexpected Message Received"
+            print ev
+  processEvent dpy ty text e
